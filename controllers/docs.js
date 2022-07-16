368c7f8014d9ca2059425c318e62c8d820a70580
Sajjad Mahmoudi <65295591+Sajjad-Mi@users.noreply.github.com>
2022-07-16 19:04:30 +0430

    Add feature to show user his documents

--- controllers/docs.js
+++ controllers/docs.js
@@ -1,12 +1,14 @@
 
 const User = require("../models/User");
 const Docs = require("../models/Docs");
-module.exports.docsList_get = (req , res) =>{
-    res.render('docslist');                                                    
+
+module.exports.docsList_get = async (req , res) =>{
+    const userDocs = await User.findOne({_id: req.id}, {docsId: 1});
+    res.render('docslist', {docs: userDocs.docsId});                                                    
 }
 module.exports.createDoc_post = async (req , res) =>{
   
-    const user = await User.findOne({_id: req.id}, {username: 1, docsId:1});
+    const user = await User.findOne({_id: req.id}, {username: 1, docsId: 1});
     const doc = await Docs.create({title: req.body.title, body:'' });  
     doc.users.push(user.username);
     user.docsId.push({title:req.body.title, _id:doc._id});

20be3471c6bcbf2eab1d09ec23e17e32b932d637
Sajjad Mahmoudi <65295591+Sajjad-Mi@users.noreply.github.com>
2022-07-26 17:53:20 +0430

    Add feature which user can log out

--- controllers/auth.js
+++ controllers/auth.js
@@ -74,3 +74,7 @@ module.exports.login_post = async (req , res) =>{
     }
      
 }
+module.exports.logout_get = (req , res) =>{
+    res.cookie('jwt', '', {maxAge: 1})
+    res.redirect('/login')
+}
--- routes/auth.js
+++ routes/auth.js
@@ -8,5 +8,6 @@ router.get("/login", authControllers.login_get);
 
 router.post("/signup", authControllers.signup_post);
 router.post("/login", authControllers.login_post);
+router.get("/logout", authControllers.logout_get);
 
 module.exports = router;
